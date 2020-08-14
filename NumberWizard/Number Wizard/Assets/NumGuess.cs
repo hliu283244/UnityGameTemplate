e912f0306d8d636394ce447344110fc4e6cc1a52
Derek <dli@e2020.com>
2020-08-13 20:55:57 -0700

    First commit for Number wizard

---
+++ NumberWizard/Number Wizard/Assets/NumGuess.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumGuess : MonoBehaviour
+{
+    
+    [SerializeField] int maxNum;
+    [SerializeField] int minNum;
+    [SerializeField] TextMeshProUGUI displayNum;
+    int guessNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startGame();
+    }
+
+    public void startGame()
+    {
+        maxNum = maxNum + 1;
+        guessANum();
+        displayGuessNumber();
+    }
+
+    public void onPressHigher()
+    {
+        if (minNum >= maxNum -1)
+        {
+            minNum = maxNum - 1;
+        }
+        else
+        {
+            minNum = guessNum + 1;
+        }
+        guessANum();
+        displayGuessNumber();
+    }
+
+    public void onPressLower()
+    {
+        if(maxNum <= minNum)
+        {
+            maxNum = minNum;
+        }
+        else
+        {
+            maxNum = guessNum;
+        }
+        guessANum();
+        displayGuessNumber();
+    }
+
+    public void guessANum()
+    {
+        Debug.Log("Min:" + minNum);
+        Debug.Log("Max:" + maxNum);
+        guessNum = Random.Range(minNum, maxNum);
+    }
+
+    public void displayGuessNumber()
+    {
+        displayNum.text = guessNum.ToString();
+    }
+    
+}
